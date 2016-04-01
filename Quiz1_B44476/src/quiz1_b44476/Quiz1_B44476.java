efbd8ef84f9cf96e7145e3ae4f33995cf2040041
Mauricio Montiel <JOCSELY@Mey>
2016-04-01 10:13:54 -0600

    primera prueba

---
+++ Quiz1_B44476/src/quiz1_b44476/Quiz1_B44476.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz1_b44476;
+
+/**
+ *
+ * @author JOCSELY
+ */
+public class Quiz1_B44476
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
